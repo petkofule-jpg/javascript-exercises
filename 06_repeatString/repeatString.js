7ffd489a8f0171e62b6afd8b3104ccfb92931fc0
Peter Füle <12345678+petkofule-jpg@users.noreply.github.com>
2025-10-27 15:30:08 +0100

    Finished 6th excercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let string = "";
+    for (let i = 0; i < num; i++) {
+        string += word;
+    }
+    return string;
 };
+
+console.log(repeatString(hey, 3))
 
 // Do not edit below this line
 module.exports = repeatString;
